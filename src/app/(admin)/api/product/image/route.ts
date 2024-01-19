5d756ac91e1ff5582fcfed9a47d372896be7523d
Eren Çifci <kleesd>
2024-01-19 13:38:25 +0300

    delete admin api folder

--- src/app/(admin)/api/product/image/route.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "@/lib/db/prisma";
-import { NextRequest, NextResponse } from "next/server";
-
-export const POST = async (req: NextRequest) => {
-    // create a new image
-
-    const { productId, url } = await req.json();
-};
