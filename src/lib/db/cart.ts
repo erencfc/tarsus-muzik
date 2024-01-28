7efddcf22f8796e743672f300ccc491dc9451a72
Eren Çifci <kleesd>
2024-01-28 03:24:45 +0300

    .

--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -188,7 +188,7 @@ export async function createCart(): Promise<ShoppingCart> {
         cookies().set("localCartId", newCart.id, {
             httpOnly: true,
             maxAge: 60 * 60 * 24 * 365,
-            // secure: process.env.NODE_ENV === "production",
+            secure: false,
         });
     }
 
