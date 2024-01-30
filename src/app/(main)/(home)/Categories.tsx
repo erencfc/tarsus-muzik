b307013b2ce79317aad255262f665192dac0c761
Eren Çifci <kleesd>
2024-01-30 07:41:56 +0300

    .

--- src/app/(main)/(home)/Categories.tsx
+++ src/app/(main)/(home)/Categories.tsx
@@ -15,40 +15,47 @@ export default async function Categories() {
                 </h4>
             </div>
             <Carousel>
-                {categories.map((category) => (
-                    <CarouselItem
-                        key={category.id}
-                        className="basis-full border p-4 sm:basis-1/2 min-[850px]:basis-1/3"
-                    >
-                        <div
+                {categories
+                    .sort((a, b) => {
+                        // order değerine göre sırala (eğer 0 ise en alta at)
+                        if (a.order === 0) return 1;
+                        if (b.order === 0) return -1;
+                        return a.order - b.order;
+                    })
+                    .map((category) => (
+                        <CarouselItem
                             key={category.id}
-                            className="flex select-none flex-col items-center justify-center"
+                            className="basis-full border p-4 sm:basis-1/2 min-[850px]:basis-1/3"
                         >
-                            <Link
-                                href={`/kategori/${category.slug}`}
-                                draggable={false}
+                            <div
+                                key={category.id}
+                                className="flex select-none flex-col items-center justify-center"
                             >
-                                <div className="group flex flex-col items-center justify-center">
-                                    <Image
-                                        className={` h-[196px] w-[196px] rounded-full border border-solid border-gray-200 transition-all duration-300 ease-in-out group-hover:scale-105 ${
-                                            category.color
-                                                ? `bg-gray-200/60 object-contain p-4`
-                                                : "object-cover"
-                                        }`}
-                                        src={`/images/category/${category.slug}.png`}
-                                        width={196}
-                                        height={196}
-                                        alt={category.name}
-                                        draggable={false}
-                                    />
-                                    <span className="mt-5 text-center font-medium text-gray-600">
-                                        {category.name}
-                                    </span>
-                                </div>
-                            </Link>
-                        </div>
-                    </CarouselItem>
-                ))}
+                                <Link
+                                    href={`/kategori/${category.slug}`}
+                                    draggable={false}
+                                >
+                                    <div className="group flex flex-col items-center justify-center">
+                                        <Image
+                                            className={` h-[196px] w-[196px] rounded-full border border-solid border-gray-200 transition-all duration-300 ease-in-out group-hover:scale-105 ${
+                                                category.color
+                                                    ? `bg-gray-200/60 object-contain p-4`
+                                                    : "object-cover"
+                                            }`}
+                                            src={`/images/category/${category.slug}.png`}
+                                            width={196}
+                                            height={196}
+                                            alt={category.name}
+                                            draggable={false}
+                                        />
+                                        <span className="mt-5 text-center font-medium text-gray-600">
+                                            {category.name}
+                                        </span>
+                                    </div>
+                                </Link>
+                            </div>
+                        </CarouselItem>
+                    ))}
             </Carousel>
         </div>
     );
